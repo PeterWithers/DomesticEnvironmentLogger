367f86c20098c6a4b111800fd8fee4cbbd566692
Peter Withers <peter@bambooradical.com>
2016-11-20 17:57:14 +0100

    Update the firmware to send data to the logger web service.

--- firmware/src/ESP-POST.cpp
+++ firmware/src/ESP-POST.cpp
@@ -16,6 +16,9 @@
 
 const char* ssid = "";
 const char* password = "";
+const char* reportingServer = "192.168.1.23";
+const int httpPort = 8084;
+String locationString = "testing%20board";
 
 #define DHTPIN        4
 #define DHTTYPE       DHT22
@@ -23,23 +26,31 @@ const char* password = "";
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
 void sendMonitoredData() {
+    String errorString = "";
+    Serial.println(reportingServer);
+    String url = "/monitor/add?temperature=";
     sensors_event_t event;
     dht.temperature().getEvent(&event);
     String telemetryString = "";
     if (isnan(event.temperature)) {
         telemetryString += "Error reading temperature<br/>";
+        errorString += "Error%20reading%20temperature. ";
     } else {
         telemetryString += "Temperature: ";
         telemetryString += event.temperature;
         telemetryString += " *C<br/>";
+        url+=event.temperature;
     }
     dht.humidity().getEvent(&event);
+    url+="&humidity=";
     if (isnan(event.relative_humidity)) {
         telemetryString += "Error reading humidity<br/>";
+        errorString += "Error%20reading%20humidity. ";
     } else {
         telemetryString += "Humidity: ";
         telemetryString += event.relative_humidity;
         telemetryString += "%<br/>";
+        url+=event.relative_humidity;
     }    
     telemetryString += "ADC: ";
     telemetryString += analogRead(A0);
@@ -47,8 +58,43 @@ void sendMonitoredData() {
     telemetryString += "voltage: ";
     telemetryString += (analogRead(A0) / 69.0);
     telemetryString += "v";
+    url+="&voltage=";
+    url+=(analogRead(A0) / 69.0);
 
-    Serial.print(telemetryString);
+    Serial.println(telemetryString);
+    WiFiClient client;
+    if (!client.connect(reportingServer, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+    url+="&location=";
+    url+=locationString;
+    url+="&error=";
+    url+= errorString;
+    Serial.println(url);
+  
+    String connectionString = "GET ";
+    connectionString += url;
+    connectionString += " HTTP/1.1\r\n";
+    connectionString += "Host: ";
+    connectionString += reportingServer;
+    connectionString +="\r\n";
+    connectionString +="Connection: close\r\n\r\n";
+    Serial.println(connectionString);
+    client.print(connectionString);
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println("timeout");
+            client.stop();
+            return;
+        }
+    }
+  
+    while(client.available()){
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
 }
 
 void setup() {
