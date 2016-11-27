5b5080bcc07180831a237874fb9475231d150c8b
Peter Withers <peter-gthb@bambooradical.com>
2016-11-27 23:05:05 +0100

    Added a second monitor board.

--- monitor/src/main/java/com/bambooradical/monitor/repository/DataRecordRepository.java
+++ monitor/src/main/java/com/bambooradical/monitor/repository/DataRecordRepository.java
@@ -5,6 +5,7 @@ package com.bambooradical.monitor.repository;
 
 import com.bambooradical.monitor.model.DataRecord;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -13,5 +14,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface DataRecordRepository extends JpaRepository<DataRecord, Long> {
 
-    List<DataRecordRepository> findByLocationStartsWithIgnoreCase(String location);
+    List<DataRecord> findByLocationStartsWithIgnoreCase(String location, final Pageable pageable);
 }
