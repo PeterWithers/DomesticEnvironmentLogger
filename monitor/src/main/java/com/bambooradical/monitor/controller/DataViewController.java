d7af463fd61d204465f9012e10cdcb248092f1b7
Peter Withers <peter-gthb@bambooradical.com>
2017-02-05 10:23:20 +0100

    Added an energy record viewer thymleaf template with listing and data input form.

--- monitor/src/main/java/com/bambooradical/monitor/controller/DataViewController.java
+++ monitor/src/main/java/com/bambooradical/monitor/controller/DataViewController.java
@@ -4,6 +4,7 @@
 package com.bambooradical.monitor.controller;
 
 import com.bambooradical.monitor.repository.DataRecordRepository;
+import com.bambooradical.monitor.repository.EnergyRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,12 +16,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class DataViewController {
+
     @Autowired
     DataRecordRepository dataRecordRepository;
+    @Autowired
+    EnergyRecordRepository energyRecordRepository;
 
     @RequestMapping("/")
     public String getChart(Model model) {
         return "dataviewer";
     }
 
+    @RequestMapping("/energy")
+    public String getEnergy(Model model) {
+        model.addAttribute("energyData", energyRecordRepository.findAll());
+        return "energyviewer";
+    }
 }
