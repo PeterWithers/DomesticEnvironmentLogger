c4dd95740a18e5327a30bb05185aa010794ec51b
Peter Withers <peter-gthb@bambooradical.com>
2017-10-14 14:13:07 +0200

    Support for cloud datastore over crud.

--- monitor/src/main/java/com/bambooradical/monitor/repository/EnergyRecordService.java
+++ monitor/src/main/java/com/bambooradical/monitor/repository/EnergyRecordService.java
@@ -4,30 +4,75 @@
 package com.bambooradical.monitor.repository;
 
 import com.bambooradical.monitor.model.EnergyRecord;
+import com.google.cloud.Timestamp;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.IncompleteKey;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 /**
  * @since Oct 14, 2017 1:31:49 PM (creation date)
  * @author : Peter Withers <peter@gthb-bambooradical.com>
  */
+@Service
 public class EnergyRecordService {
 
+    @Autowired
+    Datastore datastore;
+
+    private KeyFactory keyFactory;
+
+    @PostConstruct
+    public void initializeKeyFactories() {
+        keyFactory = datastore.newKeyFactory().setKind("EnergyRecord");
+    }
+
     public List<EnergyRecord> findAll() {
-        throw new UnsupportedOperationException();
+        List<EnergyRecord> resultList = new ArrayList<>();
+        Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind("EnergyRecord")
+                //                .setFilter(PropertyFilter.eq("RemoteAddress", "x"))
+                .build();
+        QueryResults<Entity> results = datastore.run(query);
+        while (results.hasNext()) {
+            Entity currentEntity = results.next();
+            resultList.add(new EnergyRecord(
+                    currentEntity.getString("MeterLocation"),
+                    currentEntity.getDouble("MeterValue"),
+                    new Date(currentEntity.getTimestamp("RecordDate").getSeconds() * 100L)));
+        }
+        return resultList;
     }
 
     public long count() {
         throw new UnsupportedOperationException();
     }
 
-    public EnergyRecord save(EnergyRecord energyRecord) {
-        throw new UnsupportedOperationException();
+    public Entity save(EnergyRecord energyRecord) {
+        IncompleteKey key = keyFactory.setKind("EnergyRecord").newKey();
+        FullEntity entity = FullEntity.newBuilder(key)
+                .set("MeterLocation", energyRecord.getMeterLocation())
+                .set("MeterValue", energyRecord.getMeterValue())
+                .set("RecordDate", Timestamp.of(energyRecord.getRecordDate()))
+                .build();
+        return datastore.put(entity);
+
     }
 
     public void delete(EnergyRecord energyRecord) {
-        throw new UnsupportedOperationException();
+        Key key = keyFactory.newKey(energyRecord.getId());
+        datastore.delete(key);
     }
 
     public List<EnergyRecord> findByMeterLocationOrderByRecordDateAsc(String meterLocation, final Pageable pageable) {
