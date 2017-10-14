28a46344de3332556401b74cd1f2185f54a1584b
Peter Withers <peter-gthb@bambooradical.com>
2017-10-14 14:12:01 +0200

    Support for cloud datastore over crud.

--- monitor/src/main/java/com/bambooradical/monitor/repository/DataRecordService.java
+++ monitor/src/main/java/com/bambooradical/monitor/repository/DataRecordService.java
@@ -4,30 +4,80 @@
 package com.bambooradical.monitor.repository;
 
 import com.bambooradical.monitor.model.DataRecord;
+import com.google.cloud.Timestamp;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.IncompleteKey;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 /**
  * @since Oct 14, 2017 1:32:09 PM (creation date)
  * @author : Peter Withers <peter@gthb-bambooradical.com>
  */
+@Service
 public class DataRecordService {
 
+    @Autowired
+    Datastore datastore;
+
+    private KeyFactory keyFactory;
+
+    @PostConstruct
+    public void initializeKeyFactories() {
+        keyFactory = datastore.newKeyFactory().setKind("DataRecord");
+    }
+
     public List<DataRecord> findAll() {
-        throw new UnsupportedOperationException();
+        List<DataRecord> resultList = new ArrayList<>();
+        Query<Entity> query = Query.newEntityQueryBuilder()
+                .setKind("DataRecord")
+                //                .setFilter(PropertyFilter.eq("RemoteAddress", "x"))
+                .build();
+        QueryResults<Entity> results = datastore.run(query);
+        while (results.hasNext()) {
+            Entity currentEntity = results.next();
+            resultList.add(new DataRecord(
+                    (float) currentEntity.getDouble("Temperature"),
+                    (float) currentEntity.getDouble("Humidity"),
+                    (float) currentEntity.getDouble("Voltage"),
+                    currentEntity.getString("MeterLocation"),
+                    currentEntity.getString("Error"),
+                    new Date(currentEntity.getTimestamp("RecordDate").getSeconds() * 100L)));
+        }
+        return resultList;
     }
 
     public long count() {
         throw new UnsupportedOperationException();
     }
 
-    public DataRecord save(DataRecord dataRecord) {
-        throw new UnsupportedOperationException();
+    public Entity save(DataRecord dataRecord) {
+        IncompleteKey key = keyFactory.setKind("DataRecord").newKey();
+        FullEntity entity = FullEntity.newBuilder(key)
+                .set("Error", dataRecord.getError())
+                .set("Humidity", dataRecord.getHumidity())
+                .set("Location", dataRecord.getLocation())
+                .set("Temperature", dataRecord.getTemperature())
+                .set("Voltage", dataRecord.getVoltage())
+                .set("RecordDate", Timestamp.of(dataRecord.getRecordDate()))
+                .build();
+        return datastore.put(entity);
     }
 
     public void delete(DataRecord dataRecord) {
-        throw new UnsupportedOperationException();
+        Key key = keyFactory.newKey(dataRecord.getId());
+        datastore.delete(key);
     }
 
     public List<DataRecord> findByLocationStartsWithIgnoreCaseOrderByRecordDateAsc(String location, final Pageable pageable) {
