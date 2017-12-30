a1588b1ecff3ba525ecb0acdd96f2efbf164f636
Peter Withers <peter-gthb@bambooradical.com>
2017-12-30 14:02:31 +0100

    Added a lighting service and controller.

--- monitor/src/main/java/com/bambooradical/monitor/controller/DataRecordController.java
+++ monitor/src/main/java/com/bambooradical/monitor/controller/DataRecordController.java
@@ -12,8 +12,6 @@ import com.bambooradical.monitor.repository.EnergyRecordService;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -25,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @created: 19/11/2016 23:48:12
- * @author : Peter Withers <peter@gthb-bambooradical.com>
+ * @author : Peter Withers <peter-gthb@bambooradical.com>
  */
 @RestController
 @RequestMapping("/monitor")
@@ -39,33 +37,6 @@ public class DataRecordController {
     DataRecordService dataRecordService;
     @Autowired
     EnergyRecordService energyRecordService;
-
-    static String valueRGB = "";
-
-    @RequestMapping("/currentRGB")
-    public String currentRGB(
-            @RequestParam(value = "location", required = true) String location,
-            @RequestParam(value = "value", required = false, defaultValue = "") String value
-    ) {
-        if (value != null && !value.isEmpty()) {
-            valueRGB = (value.length() > 14) ? value : "";
-        }
-        if (valueRGB.isEmpty()) {
-            DateTime dateTime = new DateTime(DateTimeZone.forOffsetHours(+1));
-            int hourOfDay = dateTime.getHourOfDay();
-            if (hourOfDay > 23) {
-                return "550055:000000;550000T000100;555500T000200;005500T000300;005555T000400;000055T000500;550055T000600;";
-            } else if (hourOfDay > 10) {
-                return "000000:000000;550000T000100;005500:000200;000055:000300;";
-            } else if (hourOfDay > 12) {
-                return "000000:000000;ff0000T000100;00ff00:000200;0000ff:000300;000000:000400;ffffffT000500;000000T000600;ffffffT000700;";
-            } else {
-                return "000000:000000;000000T000100;000000:000200;";
-            }
-        } else {
-            return valueRGB;
-        }
-    }
 
     @RequestMapping("/add")
     public DataRecord addRecord(
