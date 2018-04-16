3d2bef258570abaf37510c0e5896ffff028b3cfd
Peter Withers <peter-gthb@bambooradical.com>
2018-04-16 23:26:04 +0200

    Started a low frequency spectral monitor.

--- firmware/src/LowFrequencyMonitor.cpp
+++ firmware/src/LowFrequencyMonitor.cpp
@@ -29,8 +29,6 @@ const uint8_t amplitude = 100;
 double arrayReal[samples];
 double arrayImag[samples];
 
-int sampleIndex = 0;
-
 void setup() {
     Serial.begin(115200);
     Serial.println("LowFrequencyMonitor");
@@ -48,18 +46,18 @@ void loop() {
     double temperature = pressureSensor.readTemperature();
     double pressure = pressureSensor.readPressure();
     double altitude = pressureSensor.readAltitude(baselinePressure);
-    sampleIndex++;
-    if (sampleIndex >= samples) {
-        sampleIndex = 0;
-        Serial.print("baseline pressure: ");
-        Serial.print(temperature);
-        Serial.println(" c");
-        Serial.print(pressure);
-        Serial.println(" mb");
-        Serial.print(altitude);
-        Serial.println(" m");
-        Serial.print(millis());
-        Serial.println(" ms");
+    for (int sampleIndex = 0; sampleIndex < samples; sampleIndex++) {
+        arrayReal[sampleIndex] = pressureSensor.readPressure();
+        arrayImag[sampleIndex] = 0.0;
+        while (lastMs + 10 > millis()); /* attempt to sample at 100hz */
     }
-    while (lastMs + 10 > millis()); /* attempt to sample at 100hz */
+    Serial.print("baseline pressure: ");
+    Serial.print(temperature);
+    Serial.println(" c");
+    Serial.print(pressure);
+    Serial.println(" mb");
+    Serial.print(altitude);
+    Serial.println(" m");
+    Serial.print(millis());
+    Serial.println(" ms");
 }
