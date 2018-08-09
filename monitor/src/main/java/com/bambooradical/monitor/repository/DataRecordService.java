37513c566437c470c3643959fb4c27792c41129a
Peter Withers <peter-gthb@bambooradical.com>
2018-08-09 23:57:24 +0200

    Decreased the number of days data to insert into the daily peeks in one request.

--- monitor/src/main/java/com/bambooradical/monitor/repository/DataRecordService.java
+++ monitor/src/main/java/com/bambooradical/monitor/repository/DataRecordService.java
@@ -298,7 +298,7 @@ public class DataRecordService {
         for (LocalDate date = startDate; date.isBefore(endDate); date = date.plusDays(1)) {
             String dateKey = date.toString("yyyy-MM-dd");
             if (!DAILY_PEEKS.containsKey(dateKey + "_" + location)) {
-                if (insertedDays < 30) { // only insert only 30 days data in one request
+                if (insertedDays < 14) { // only insert only 14 days data in one request
                     insertDailyPeeks(location, dateKey, date, resultList);
                     insertedDays++;
                 }
