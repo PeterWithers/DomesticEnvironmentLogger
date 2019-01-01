72d8f1dc2f8d09dfd7c835701fc53c016c442a8f
Peter Withers <peter-gthb@bambooradical.com>
2019-01-01 21:59:42 +0100

    Started to add functionality to collect data from the CV and post it to the monitor service.

---
+++ firmware/src/CvData.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2019 Peter Withers
+ */
+
+
+/*
+ * File:   CvData.cpp
+ * Author : Peter Withers <peter-gthb@bambooradical.com>
+ *
+ * Created on January 1, 2019, 21:48 PM
+ */
+#include <stdio.h>
+#include <string.h>
+#include "CvData.h"
+
+void CvData::getCvData() {
+}
+
+void CvData::postCvData() {
+}
