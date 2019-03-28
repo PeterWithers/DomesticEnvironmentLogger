3d8fce7defcc330bef77a3d909d89530468c6f38
Peter Withers <peter-gthb@bambooradical.com>
2019-03-28 22:52:16 +0100

    Started adding JS to consume the daily overview JSON.

--- monitor/src/main/webapp/overviewdata.js
+++ monitor/src/main/webapp/overviewdata.js
@@ -5,3 +5,65 @@
  @author @author : Peter Withers <peter-gthb@bambooradical.com>
  */
 
+$("<canvas id=\"overviewContainer\" width=\"800\" height=\"400\"></canvas>").appendTo("body");
+var overviewContainer = $("#overviewContainer");
+var overviewChart = new Chart(overviewContainer, {
+    type: 'line',
+    data: {
+        datasets: []
+    },
+    options: {
+        bezierCurve: false,
+        responsive: true,
+        maintainAspectRatio: true,
+        scales: {
+            xAxes: [{
+                    type: 'time',
+                    time: {
+                        displayFormats: {
+                            quarter: 'YYYY MMM D H:mm:ss'
+                        },
+                        tooltipFormat: 'YYYY MMM D H:mm:ss'
+                    }
+                }]}
+    }});
+$.getJSON("/monitor/overview", function (locationData) {
+    var labels = [];
+    var labelsDone = false;
+    $.each(locationData, function (locationKey, channelData) {
+        $.each(channelData, function (channelKey, yearMonthData) {
+            var setKeyFilter = "avg";
+            var dataValues = [];
+            $.each(yearMonthData, function (yearMonthKey, setOfData) {
+                $.each(setOfData, function (setKey, daysOfData) {
+                    if (setKey === setKeyFilter) {
+                        $.each(daysOfData, function (index, daysValue) {
+                            if (!labelsDone) {
+                                var yearMonthParts = yearMonthKey.split("-");
+                                labels.push(new Date(yearMonthParts[0], parseInt(yearMonthParts[1]) - 1, index));
+                                dataValues.push(daysValue);
+                            }
+                        });
+                    }
+                });
+            });
+            if (!labelsDone) {
+                overviewChart.data.labels = labels;
+            }
+            labelsDone = true;
+            overviewChart.data.datasets.push({
+                label: locationKey + " " + channelKey + " " + setKeyFilter,
+                data: dataValues,
+                radius: 1,
+                borderWidth: 1,
+                spanGaps: true,
+                backgroundColor: "rgba(255,99,132,0.2)",
+                borderColor: "rgba(255,99,132,1)",
+                pointBackgroundColor: "rgba(255,99,132,1)",
+                pointBorderColor: "#fff",
+                pointHoverBackgroundColor: "#fff",
+                pointHoverBorderColor: "rgba(255,99,132,1)"
+            });
+        });
+    });
+});
