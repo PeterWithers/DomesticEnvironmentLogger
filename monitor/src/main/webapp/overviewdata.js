1fbf62ded88075037733985931f547e1b30afdab
Peter Withers <peter-gthb@bambooradical.com>
2019-03-30 13:29:03 +0100

    Adding JS that consumes the daily overview JSON.

--- monitor/src/main/webapp/overviewdata.js
+++ monitor/src/main/webapp/overviewdata.js
@@ -6,6 +6,55 @@
  */
 var overviewChart;
 var graphDataChannels = {};
+var channelColours = {
+    'Temperature 2': {label: 'Temperature 2',
+        backgroundColor: "rgba(179,181,198,0.2)",
+        borderColor: "rgba(179,181,198,1)"},
+    'Temperature 1': {label: 'Temperature 1',
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)"},
+    'Temperature 3': {
+        label: 'Temperature 3',
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)"},
+    'Temperature 4': {
+        label: 'Temperature 4',
+        backgroundColor: "rgba(75, 92, 192, 0.2)",
+        borderColor: "rgba(75, 92, 192, 1)"},
+    'aquariuma0': {
+        label: 'AquariumA',
+        backgroundColor: "rgba(75, 92, 192, 0.0)",
+        borderColor: "rgba(75, 92, 192, 1)"},
+    'aquariuma1': {
+        label: 'AquariumB',
+        backgroundColor: "rgba(95, 52, 192, 0.0)",
+        borderColor: "rgba(95, 52, 192, 1)"},
+    'rearwall top floor0': {
+        label: 'rearwall0',
+        backgroundColor: "rgba(200,100,200, 0.2)",
+        borderColor: "rgba(200,100,200, 1)"},
+    'rearwall top floor1': {
+        label: 'rearwall1',
+        backgroundColor: "rgba(180,100,200, 0.2)",
+        borderColor: "rgba(180,100,200, 1)"},
+    'rearwall top floor2': {
+        label: 'rearwall2',
+        backgroundColor: "rgba(160,100,200, 0.2)",
+        borderColor: "rgba(160,100,200, 1)"},
+    'frontwall top floor0': {
+        label: 'frontwall0',
+        backgroundColor: "rgba(200,200,100, 0.2)",
+        borderColor: "rgba(200,200,100, 1)"},
+    'frontwall top floor1': {
+        label: 'frontwall1',
+        backgroundColor: "rgba(180,200,100, 0.2)",
+        borderColor: "rgba(180,200,100, 1)"},
+    'frontwall top floor2': {
+        label: 'frontwall2',
+        backgroundColor: "rgba(160,200,100, 0.2)",
+        borderColor: "rgba(160,200,100, 1)"}
+};
+
 $(document).ready(function () {
     $("<canvas id=\"overviewContainer\" width=\"800\" height=\"400\"></canvas>").appendTo("body");
     var overviewContainer = $("#overviewContainer");
@@ -43,52 +92,52 @@ $(document).ready(function () {
                         });
                     });
                 });
-                $("<button onclick=\"addChannel('" + locationChannel + "')\">" + locationKey + " " + channelKey + "</button>").appendTo("body");
+                $("<button onclick=\"addChannel('" + locationKey + "', '" + locationChannel + "')\">" + locationKey + " " + channelKey + "</button>").appendTo("body");
             });
         });
     });
 });
-function addChannel(locationChannel) {
+function addChannel(locationKey, locationChannel) {
     overviewChart.data.datasets.push({
         label: locationChannel + " avg",
         data: graphDataChannels[locationChannel].avg,
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
+        backgroundColor: channelColours[locationKey].backgroundColor,
+        borderColor: channelColours[locationKey].borderColor,
         fill: false
     });
     overviewChart.data.datasets.push({
         label: locationChannel + " min",
         data: graphDataChannels[locationChannel].min,
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
+        backgroundColor: channelColours[locationKey].backgroundColor,
+        borderColor: channelColours[locationKey].borderColor,
         fill: false
     });
     overviewChart.data.datasets.push({
         label: locationChannel + " Q1",
         data: graphDataChannels[locationChannel].Q1,
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
+        backgroundColor: channelColours[locationKey].backgroundColor,
+        borderColor: channelColours[locationKey].borderColor,
         fill: +1
     });
     overviewChart.data.datasets.push({
         label: locationChannel + " Q2",
         data: graphDataChannels[locationChannel].Q2,
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
+        backgroundColor: channelColours[locationKey].backgroundColor,
+        borderColor: channelColours[locationKey].borderColor,
         fill: -1
     });
     overviewChart.data.datasets.push({
         label: locationChannel + " Q3",
         data: graphDataChannels[locationChannel].Q3,
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
+        backgroundColor: channelColours[locationKey].backgroundColor,
+        borderColor: channelColours[locationKey].borderColor,
         fill: -1
     });
     overviewChart.data.datasets.push({
         label: locationChannel + " max",
         data: graphDataChannels[locationChannel].max,
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)",
+        backgroundColor: channelColours[locationKey].backgroundColor,
+        borderColor: channelColours[locationKey].borderColor,
         fill: false
     });
     overviewChart.update();
