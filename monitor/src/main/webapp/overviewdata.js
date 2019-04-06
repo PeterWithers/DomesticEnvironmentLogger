a4a2a76296fd23bf0292200da2cd437ca0110e40
Peter Withers <peter-gthb@bambooradical.com>
2019-04-06 20:12:49 +0200

    Added details to the daily overview output.

--- monitor/src/main/webapp/overviewdata.js
+++ monitor/src/main/webapp/overviewdata.js
@@ -7,52 +7,176 @@
 var overviewChart;
 var graphDataChannels = {};
 var channelColours = {
-    'Temperature 2': {label: 'Temperature 2',
-        backgroundColor: "rgba(179,181,198,0.2)",
-        borderColor: "rgba(179,181,198,1)"},
-    'Temperature 1': {label: 'Temperature 1',
-        backgroundColor: "rgba(255,99,132,0.2)",
-        borderColor: "rgba(255,99,132,1)"},
-    'Temperature 3': {
-        label: 'Temperature 3',
-        backgroundColor: "rgba(75, 192, 192, 0.2)",
-        borderColor: "rgba(75, 192, 192, 1)"},
-    'Temperature 4': {
-        label: 'Temperature 4',
-        backgroundColor: "rgba(75, 92, 192, 0.2)",
-        borderColor: "rgba(75, 92, 192, 1)"},
-    'aquariuma0': {
-        label: 'AquariumA',
-        backgroundColor: "rgba(75, 92, 192, 0.0)",
-        borderColor: "rgba(75, 92, 192, 1)"},
-    'aquariuma1': {
-        label: 'AquariumB',
-        backgroundColor: "rgba(95, 52, 192, 0.0)",
-        borderColor: "rgba(95, 52, 192, 1)"},
+//    'Temperature 2': {label: 'Temperature 2',
+//        backgroundColor: "rgba(179,181,198,0.2)",
+//        borderColor: "rgba(179,181,198,1)"},
+//    'Temperature 1': {label: 'Temperature 1',
+//        backgroundColor: "rgba(255,99,132,0.2)",
+//        borderColor: "rgba(255,99,132,1)"},
+//    'Temperature 3': {
+//        label: 'Temperature 3',
+//        backgroundColor: "rgba(75, 192, 192, 0.2)",
+//        borderColor: "rgba(75, 192, 192, 1)"},
+//    'Temperature 4': {
+//        label: 'Temperature 4',
+//        backgroundColor: "rgba(75, 92, 192, 0.2)",
+//        borderColor: "rgba(75, 92, 192, 1)"},
+//    'aquariuma0': {
+//        label: 'AquariumA',
+//        backgroundColor: "rgba(75, 92, 192, 0.0)",
+//        borderColor: "rgba(75, 92, 192, 1)"},
+//    'aquariuma1': {
+//        label: 'AquariumB',
+//        backgroundColor: "rgba(95, 52, 192, 0.0)",
+//        borderColor: "rgba(95, 52, 192, 1)"},
+//    'rearwall top floor0': {
+//        label: 'rearwall0',
+//        backgroundColor: "rgba(200,100,200, 0.2)",
+//        borderColor: "rgba(200,100,200, 1)"},
+//    'rearwall top floor1': {
+//        label: 'rearwall1',
+//        backgroundColor: "rgba(180,100,200, 0.2)",
+//        borderColor: "rgba(180,100,200, 1)"},
+//    'rearwall top floor2': {
+//        label: 'rearwall2',
+//        backgroundColor: "rgba(160,100,200, 0.2)",
+//        borderColor: "rgba(160,100,200, 1)"},
+//    'frontwall top floor0': {
+//        label: 'frontwall0',
+//        backgroundColor: "rgba(200,200,100, 0.2)",
+//        borderColor: "rgba(200,200,100, 1)"},
+//    'frontwall top floor1': {
+//        label: 'frontwall1',
+//        backgroundColor: "rgba(180,200,100, 0.2)",
+//        borderColor: "rgba(180,200,100, 1)"},
+//    'frontwall top floor2': {
+//        label: 'frontwall2',
+//        backgroundColor: "rgba(160,200,100, 0.2)",
+//        borderColor: "rgba(160,200,100, 1)"}
+    'invalid:second top floor': {
+        label: 'invalid:second top floor',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'manualtest1': {
+        label: 'manualtest1',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'manualtest0': {
+        label: 'manualtest0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
     'rearwall top floor0': {
-        label: 'rearwall0',
-        backgroundColor: "rgba(200,100,200, 0.2)",
-        borderColor: "rgba(200,100,200, 1)"},
-    'rearwall top floor1': {
-        label: 'rearwall1',
-        backgroundColor: "rgba(180,100,200, 0.2)",
-        borderColor: "rgba(180,100,200, 1)"},
+        label: 'rearwall top floor0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
     'rearwall top floor2': {
-        label: 'rearwall2',
-        backgroundColor: "rgba(160,100,200, 0.2)",
-        borderColor: "rgba(160,100,200, 1)"},
-    'frontwall top floor0': {
-        label: 'frontwall0',
-        backgroundColor: "rgba(200,200,100, 0.2)",
-        borderColor: "rgba(200,200,100, 1)"},
-    'frontwall top floor1': {
-        label: 'frontwall1',
-        backgroundColor: "rgba(180,200,100, 0.2)",
-        borderColor: "rgba(180,200,100, 1)"},
+        label: 'rearwall top floor2',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'third ground floor0': {
+        label: 'third ground floor0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'rearwall top floor1': {
+        label: 'rearwall top floor1',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'aquarium0': {
+        label: 'aquarium0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'aquariuma1': {
+        label: 'aquariuma1',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'aquariuma0': {
+        label: 'aquariuma0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'third testing messaging': {
+        label: 'third testing messaging',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'second testing board': {
+        label: 'second testing board',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'aquariumb10': {
+        label: 'aquariumb10',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'aquariuma1-manually-entered-data': {
+        label: 'aquariuma1-manually-entered-data',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'rearwall top floor': {
+        label: 'rearwall top floor',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'second top floor': {
+        label: 'second top floor',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'second top floor0': {
+        label: 'second top floor0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
     'frontwall top floor2': {
-        label: 'frontwall2',
-        backgroundColor: "rgba(160,200,100, 0.2)",
-        borderColor: "rgba(160,200,100, 1)"}
+        label: 'frontwall top floor2',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'frontwall top floor1': {
+        label: 'frontwall top floor1',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'frontwall top floor0': {
+        label: 'frontwall top floor0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'second top floora0': {
+        label: 'second top floora0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'aquariumb0': {
+        label: 'aquariumb0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'aquarium': {
+        label: 'aquarium',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'deelen': {
+        label: 'deelen',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'precipitationdeelen': {
+        label: 'precipitationdeelen',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'testing first floor': {
+        label: 'testing first floor',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'testing first floor0': {
+        label: 'testing first floor0',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'testing board': {
+        label: 'testing board',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'invalid-second top floor': {
+        label: 'invalid-second top floor',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'third ground floor': {
+        label: 'third ground floor',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
+    'windspeeddeelen': {
+        label: 'windspeeddeelen',
+        backgroundColor: "rgba(123,123,123, 0.2)",
+        borderColor: "rgba(123,123,123, 1)"},
 };
 
 $(document).ready(function () {
@@ -83,19 +207,24 @@ $(document).ready(function () {
     $("<tr id=\"headerRow\"><td id=\"emptyCell\"/></tr>").appendTo("#buttonsTable");
     $.getJSON("/monitor/overview", function (locationData) {
         $.each(locationData, function (locationKey, channelData) {
-            $("<tr id=\"" + locationKey + "\"><td>" + locationKey + "</td></tr>").appendTo("#buttonsTable");
+            var locationId = locationKey.replace(/ /g, "_");
+            $("<tr id=\"" + locationId + "\"><td>" + locationKey + "</td></tr>").appendTo("#buttonsTable");
             for (var value in columnLabels) {
-                $("<td id=\"" + columnLabels[value] + "_" + locationKey + "\">").appendTo("#" + locationKey);
+                $("<td id=\"" + columnLabels[value] + "_" + locationId + "\">").appendTo("#" + locationId);
             }
+//            $("<div>'" + locationKey + "': {</div>").appendTo("body");
+//        $("<div>label: '" + locationKey + "',</div>").appendTo("body");
+//        $("<div>backgroundColor: \"rgba(123,123,123, 0.2)\",</div>").appendTo("body");
+//        $("<div>borderColor: \"rgba(123,123,123, 1)\"},</div>").appendTo("body");
             $.each(channelData, function (channelKey, yearMonthData) {
                 if (columnLabels.indexOf(channelKey) < 0) {
                     columnLabels.push(channelKey);
                     $("<td>" + channelKey + "</td>").appendTo("#headerRow");
-                    $("<td id=\"" + channelKey + "_" + locationKey + "\"/>").appendTo("#" + locationKey);
+                    $("<td id=\"" + channelKey + "_" + locationId + "\"/>").appendTo("#" + locationId);
                 }
-                var locationChannel = (locationKey + "_" + channelKey).split(" ").join("_");
+                var locationChannel = (locationId + "_" + channelKey).split(" ").join("_");
                 graphDataChannels[locationChannel] = {"avg": [], "min": [], "Q1": [], "Q2": [], "Q3": [], "max": []};
-                Object.keys(yearMonthData).sort().forEach(function(yearMonthKey) {
+                Object.keys(yearMonthData).sort().forEach(function (yearMonthKey) {
                     $.each(yearMonthData[yearMonthKey], function (setKey, daysOfData) {
                         $.each(daysOfData, function (index, daysValue) {
                             if (daysValue > 0) {
@@ -106,7 +235,7 @@ $(document).ready(function () {
                         });
                     });
                 });
-                $("<button onclick=\"addChannel('" + locationKey + "', '" + locationChannel + "')\">" + locationKey + " " + channelKey + "</button>").appendTo("#" + channelKey + "_" + locationKey);
+                $("<button onclick=\"addChannel('" + locationKey + "', '" + locationChannel + "')\">" + locationKey + " " + channelKey + "</button>").appendTo("#" + channelKey + "_" + locationId);
             });
         });
     });
