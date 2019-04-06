f22c9a90a547b703238b468936c4e6501b8754b8
Peter Withers <peter-gthb@bambooradical.com>
2019-04-06 21:39:19 +0200

    Added details to the daily overview output.

--- monitor/src/main/webapp/overviewdata.js
+++ monitor/src/main/webapp/overviewdata.js
@@ -67,20 +67,20 @@ var channelColours = {
         borderColor: "rgba(123,123,123, 1)"},
     'rearwall top floor0': {
         label: 'rearwall top floor0',
-        backgroundColor: "rgba(123,123,123, 0.2)",
-        borderColor: "rgba(123,123,123, 1)"},
+        backgroundColor: "rgba(200,100,200, 0.2)",
+        borderColor: "rgba(200,100,200, 1)"},
     'rearwall top floor2': {
         label: 'rearwall top floor2',
-        backgroundColor: "rgba(123,123,123, 0.2)",
-        borderColor: "rgba(123,123,123, 1)"},
+        backgroundColor: "rgba(160,100,200, 0.2)",
+        borderColor: "rgba(160,100,200, 1)"},
     'third ground floor0': {
         label: 'third ground floor0',
         backgroundColor: "rgba(75, 192, 192, 0.2)",
         borderColor: "rgba(75, 192, 192, 1)"},
     'rearwall top floor1': {
         label: 'rearwall top floor1',
-        backgroundColor: "rgba(123,123,123, 0.2)",
-        borderColor: "rgba(123,123,123, 1)"},
+        backgroundColor: "rgba(180,100,200, 0.2)",
+        borderColor: "rgba(180,100,200, 1)"},
     'aquarium0': {
         label: 'aquarium0',
         backgroundColor: "rgba(75, 92, 192, 0.2)",
@@ -111,8 +111,8 @@ var channelColours = {
         borderColor: "rgba(123,123,123, 1)"},
     'rearwall top floor': {
         label: 'rearwall top floor',
-        backgroundColor: "rgba(123,123,123, 0.2)",
-        borderColor: "rgba(123,123,123, 1)"},
+        backgroundColor: "rgba(200,100,200, 0.2)",
+        borderColor: "rgba(200,100,200, 1)"},
     'second top floor': {
         label: 'second top floor',
         backgroundColor: "rgba(179,181,198, 0.2)",
@@ -123,16 +123,16 @@ var channelColours = {
         borderColor: "rgba(179,181,198, 1)"},
     'frontwall top floor2': {
         label: 'frontwall top floor2',
-        backgroundColor: "rgba(123,123,123, 0.2)",
-        borderColor: "rgba(123,123,123, 1)"},
+        backgroundColor: "rgba(160,200,100, 0.2)",
+        borderColor: "rgba(160,200,100, 1)"},
     'frontwall top floor1': {
         label: 'frontwall top floor1',
-        backgroundColor: "rgba(123,123,123, 0.2)",
-        borderColor: "rgba(123,123,123, 1)"},
+        backgroundColor: "rgba(180,200,100, 0.2)",
+        borderColor: "rgba(180,200,100, 1)"},
     'frontwall top floor0': {
         label: 'frontwall top floor0',
-        backgroundColor: "rgba(123,123,123, 0.2)",
-        borderColor: "rgba(123,123,123, 1)"},
+        backgroundColor: "rgba(200,200,100, 0.2)",
+        borderColor: "rgba(200,200,100, 1)"},
     'second top floora0': {
         label: 'second top floora0',
         backgroundColor: "rgba(179,181,198, 0.2)",
@@ -209,7 +209,8 @@ $(document).ready(function () {
     $("<table id=\"buttonsTable\"/>").appendTo("body");
     $("<tr id=\"headerRow\"><td id=\"emptyCell\"/></tr>").appendTo("#buttonsTable");
     $.getJSON("/monitor/overview", function (locationData) {
-        $.each(locationData, function (locationKey, channelData) {
+        Object.keys(locationData).sort().forEach(function (locationKey) {
+            var channelData = locationData[locationKey];
             var locationId = locationKey.replace(/ /g, "_");
             $("<tr id=\"" + locationId + "\"><td>" + locationKey + "</td></tr>").appendTo("#buttonsTable");
             for (var value in columnLabels) {
@@ -238,12 +239,15 @@ $(document).ready(function () {
                         });
                     });
                 });
-                $("<button style=\"border: " + channelColours[locationKey].borderColor + " 3px solid; background:" + channelColours[locationKey].backgroundColor + ";\" onclick=\"addChannel('" + locationKey + "', '" + locationChannel + "')\">" + locationKey + " " + channelKey + "</button>").appendTo("#" + channelKey + "_" + locationId);
+                $("<button style=\"border: " + channelColours[locationKey].borderColor + " 3px solid; background:" + channelColours[locationKey].backgroundColor + ";\" onclick=\"addChannelMin('" + locationKey + "', '" + locationChannel + "')\">min</button>").appendTo("#" + channelKey + "_" + locationId);
+                $("<button style=\"border: " + channelColours[locationKey].borderColor + " 3px solid; background:" + channelColours[locationKey].backgroundColor + ";\" onclick=\"addChannelAvg('" + locationKey + "', '" + locationChannel + "')\">avg</button>").appendTo("#" + channelKey + "_" + locationId);
+                $("<button style=\"border: " + channelColours[locationKey].borderColor + " 3px solid; background:" + channelColours[locationKey].backgroundColor + ";\" onclick=\"addChannelMax('" + locationKey + "', '" + locationChannel + "')\">max</button>").appendTo("#" + channelKey + "_" + locationId);
+                $("<button style=\"border: " + channelColours[locationKey].borderColor + " 3px solid; background:" + channelColours[locationKey].backgroundColor + ";\" onclick=\"addChannelQ123('" + locationKey + "', '" + locationChannel + "')\">Q1 Q2 Q3</button>").appendTo("#" + channelKey + "_" + locationId);
             });
         });
     });
 });
-function addChannel(locationKey, locationChannel) {
+function addChannelAvg(locationKey, locationChannel) {
     overviewChart.data.datasets.push({
         label: locationChannel + " avg",
         data: graphDataChannels[locationChannel].avg,
@@ -251,6 +255,9 @@ function addChannel(locationKey, locationChannel) {
         borderColor: channelColours[locationKey].borderColor,
         fill: false
     });
+    overviewChart.update();
+}
+function addChannelMin(locationKey, locationChannel) {
     overviewChart.data.datasets.push({
         label: locationChannel + " min",
         data: graphDataChannels[locationChannel].min,
@@ -258,6 +265,9 @@ function addChannel(locationKey, locationChannel) {
         borderColor: channelColours[locationKey].borderColor,
         fill: false
     });
+    overviewChart.update();
+}
+function addChannelQ123(locationKey, locationChannel) {
     overviewChart.data.datasets.push({
         label: locationChannel + " Q1",
         data: graphDataChannels[locationChannel].Q1,
@@ -279,6 +289,9 @@ function addChannel(locationKey, locationChannel) {
         borderColor: channelColours[locationKey].borderColor,
         fill: -1
     });
+    overviewChart.update();
+}
+function addChannelMax(locationKey, locationChannel) {
     overviewChart.data.datasets.push({
         label: locationChannel + " max",
         data: graphDataChannels[locationChannel].max,
